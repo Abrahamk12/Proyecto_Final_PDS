aac5c5e06931faabe4427e92b1c869d2060122e5
Julian F. Fuentes T <56417636+SaGiT2711@users.noreply.github.com>
2022-05-04 21:55:10 -0700

    Avance a subir_calificaciones

--- Proyecto_Final_PDS/subir_calificaciones.Designer.cs
+++ Proyecto_Final_PDS/subir_calificaciones.Designer.cs
@@ -28,22 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nom_completo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SubirBT = new System.Windows.Forms.Button();
+            this.LimpiarBT = new System.Windows.Forms.Button();
+            this.SalirBT = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del alumno";
+            // 
+            // nom_completo
+            // 
+            this.nom_completo.Location = new System.Drawing.Point(149, 13);
+            this.nom_completo.Name = "nom_completo";
+            this.nom_completo.Size = new System.Drawing.Size(326, 26);
+            this.nom_completo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Materia";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Parcial";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // SubirBT
+            // 
+            this.SubirBT.Location = new System.Drawing.Point(222, 107);
+            this.SubirBT.Name = "SubirBT";
+            this.SubirBT.Size = new System.Drawing.Size(75, 32);
+            this.SubirBT.TabIndex = 6;
+            this.SubirBT.Text = "Subir";
+            this.SubirBT.UseVisualStyleBackColor = true;
+            // 
+            // LimpiarBT
+            // 
+            this.LimpiarBT.Location = new System.Drawing.Point(319, 107);
+            this.LimpiarBT.Name = "LimpiarBT";
+            this.LimpiarBT.Size = new System.Drawing.Size(75, 32);
+            this.LimpiarBT.TabIndex = 7;
+            this.LimpiarBT.Text = "Limpiar";
+            this.LimpiarBT.UseVisualStyleBackColor = true;
+            // 
+            // SalirBT
+            // 
+            this.SalirBT.Location = new System.Drawing.Point(414, 107);
+            this.SalirBT.Name = "SalirBT";
+            this.SalirBT.Size = new System.Drawing.Size(75, 32);
+            this.SalirBT.TabIndex = 8;
+            this.SalirBT.Text = "Salir";
+            this.SalirBT.UseVisualStyleBackColor = true;
             // 
             // subir_calificaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 396);
+            this.ClientSize = new System.Drawing.Size(501, 151);
+            this.Controls.Add(this.SalirBT);
+            this.Controls.Add(this.LimpiarBT);
+            this.Controls.Add(this.SubirBT);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nom_completo);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "subir_calificaciones";
             this.Text = "Subir Calificaciones";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nom_completo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button SubirBT;
+        private System.Windows.Forms.Button LimpiarBT;
+        private System.Windows.Forms.Button SalirBT;
     }
 }
